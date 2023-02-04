606683db96808ee08b3497b67ce730a444b6b39c
tijnsteenmetz <tijn.steenmetz@gmail.com>
2023-02-04 16:32:30 +0100

    Bo zijn code vervangen voor Tijn zijn code

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,81 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import './qrscreen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bounce',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'ROOK',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 0, 0)),
+        ),
+        home: MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Bounce'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedScreenIndex = 0;
-
-  void _onBackButtonTapped() {
-    setState(() {
-      if (_selectedScreenIndex == 0) {
-        _selectedScreenIndex = 1;
-      } else {
-        _selectedScreenIndex = 0;
-      }
-    });
-  }
-
-  static final List<Widget> _screenOptions = <Widget>[
-    const Text('show qr screen'),
-    const QrScreen(),
-  ];
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
     return Scaffold(
-      floatingActionButton: IconButton(
-        icon: _selectedScreenIndex == 0
-            ? const Icon(Icons.arrow_forward)
-            : const Icon(Icons.arrow_back),
-        onPressed: _onBackButtonTapped,
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        actions: [
+          Icon(
+            size: 33.5,
+            Icons.account_circle_outlined,
+            color: Colors.white,
+          )
+        ],
       ),
-      floatingActionButtonLocation: _selectedScreenIndex == 0
-          ? FloatingActionButtonLocation.endTop
-          : FloatingActionButtonLocation.startTop,
-      body: Center(
-        child: _screenOptions.elementAt(_selectedScreenIndex),
+      body: Column(
+        children: [
+          Text('A Kanker goed idea:'),
+          Text(appState.current.asLowerCase),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
+        backgroundColor: Colors.white,
+        elevation: 10.0,
+        child: const Icon(Icons.qr_code_2),
+        onPressed: () async {
+          await showDialog(context: context, builder: (_) => QrDialog());
+        },
+      ),
+    );
+  }
+}
+
+class QrDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        height: 400.0,
+        width: 400.0,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/qr.png'), fit: BoxFit.cover)),
       ),
     );
   }
